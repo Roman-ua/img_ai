ef2f769af0fe2c04732a8c2bff6e8c59034051ed
roman148 <roman@u-coders.team>
2023-05-27 01:51:17 +0300

    initial comit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-emerald-600 text-3xl font-bold underline text-red-600">
+      Hello
+    </div>
+  )
+}
+
+export default App
